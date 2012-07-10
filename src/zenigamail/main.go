9a4b63ebe7f597cd3d6d030d718982473a4fff53
Takuya Ueda <uedatakuya@gmail.com>
2012-07-10 20:27:17 +0900

    Create sample templates of bootstrap.

--- src/zenigamail/main.go
+++ src/zenigamail/main.go
@@ -1,5 +1,23 @@
 package zenigamail
 
+import (
+	"net/http"
+	"text/template"
+	"log"
+)
+
 // Initialize application.
 func init() {
+	http.HandleFunc("/", index)
+}
+
+// html templates
+var templates = template.Must(template.ParseGlob("template/*.html"))
+
+// index handle function.
+func index(w http.ResponseWriter, r *http.Request) {
+    if err := templates.ExecuteTemplate(w, "index", nil); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        log.Fatal(err.Error())
+    }	
 }
